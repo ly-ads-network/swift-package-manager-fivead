4c0fd7052d5c0160915defe7c7990b3c3deefde0
ap-screwdriver <ml-service-ap-screwdriver@lycorp.co.jp>
2026-07-06 12:29:50 +0900

    Updating Package.swift for SDK: 3.1.0-beta.4

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FiveAd",
-            url: "https://github.com/ly-ads-network/swift-package-manager-fivead/releases/download/3.1.0-beta.3/FiveAd-3.1.0-beta.3.xcframework.zip",
-            checksum: "08377eae543b2eb0a3fe7544bdb427c4550498662adbc5240cdb44c2f80a6c5e"
+            url: "https://github.com/ly-ads-network/swift-package-manager-fivead/releases/download/3.1.0-beta.4/FiveAd-3.1.0-beta.4.xcframework.zip",
+            checksum: "b8dc8a8c01bfd1734629bffdcad0e22989461cf8816d76295c09a2c621c7adf4"
         )
     ]
 )
